3453e70f1dce3d008a485c6c6e7d31b6237ca680
Georgios Tsotsos <tsotsos@gmail.com>
2019-08-04 16:34:58 +0300

    Adds Doxygen variables for doxyfile.
    Implements doxygen file generation via template and
    execution of doxygen on the target folder (docspath)
    
    Signed-off-by: Georgios Tsotsos <tsotsos@gmail.com>

--- xml2rst.py
+++ xml2rst.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import os
-
+import sys
+import string
+import shutil
+import subprocess
 
 def check_not_empty(text):
 
@@ -33,21 +36,52 @@ def check_dir(root_path, text):
             break
     return realpath
 
+def doxyfilegen ( template, vals, srcpath, docspath ):
+    vals = {
+            "PROJECT_NAME":vals["PROJECT_NAME"],
+            "PROJECT_NUMBER":vals["VERSION"]+vals["EXTVERSION"],
+            "PROJECT_BRIEF":vals["DESC"],
+            "PROJECT_LOGO":"", # to be added
+            "OUTPUT_DIRECTORY":"output",
+            "OUTPUT_LANGUAGE":"English",
+            "SRC":srcpath,
+            "EXCLUDE":""
+            }
+    #copy doxygen template to docs dir
+    doxyfile = docspath+"/Doxyfile"
+    shutil.copy2(template, doxyfile)
+
+    f1 = open(template, "r")
+    f2 = open(doxyfile, "w")
+    for line in f1:
+        for key,val in vals.items():
+            line = line.replace("$$$"+key+"$$$", val)
+            #print(line)
+        f2.write(line)
+    f1.close()
+    f2.close()
+
 def wizzard ( ):
     vals = dict()
-    version_file = "version"
+    version_file = "VERSIONS"
 
-    ROOTPATH = check_dir(False, "Enter the Project's root path: ")
-    SRCPATH = check_dir( ROOTPATH, "Enter src path (relative to root path): ")
-    DOCSPATH = check_dir( ROOTPATH, "Enter docs path (relative to root path): ")
+    # retrieve paths and normalize relative to docs path
+    rootpath = check_dir(False, "Enter the Project's root path: ")
+
+    srcpath = check_dir( rootpath, "Enter src path (relative to root path): ")
+    docspath = check_dir( rootpath, "Enter docs path (relative to root path): ")
+    rel_srcpath = os.path.relpath(srcpath,docspath)
     vals["PROJECT_NAME"] = check_not_empty("Project Name: ")
+    vals["DESC"] = check_not_empty("Project description: ")
     vals["VERSION"] = check_not_empty("Release: ")
     vals["EXTVERSION"] = input("Extra version eg. -rc-1 (optional): ")
     vals["AUTHOR"] = check_not_empty("Author(s) :")
     vals["CREDITS"] = check_not_empty(
         "Initial Contributor (later you can add more):")
 
-    version_file = os.path.join(ROOTPATH, version_file)
+    version_file = os.path.join(rootpath, version_file)
+    xml2rst_file = os.path.join(rootpath,docspath)
+    xml2rst_file = os.path.join(xml2rst_file,"XML2RST")
 
     if os.path.isfile(version_file):
         print("File " + version_file +
@@ -57,6 +91,16 @@ def wizzard ( ):
     with open(version_file, 'w') as data:
         for key, val in vals.items():
             data.write('%s=%s\n' % (key, val))
+    data.close()
+
+    with open(xml2rst_file, 'w') as data:
+        data.write("SRC = " + rel_srcpath + "\n")
+        data.write("docspath = " + docspath + "\n")
+    data.close()
+    #generate Doxyfile
+    doxyfilegen("templates/Doxyfile.template",vals,rel_srcpath,docspath)
+    #run doxygen Doxyfile to docs path
+    subprocess.Popen(["doxygen",docspath+"/Doxyfile"], cwd=docspath)
 
 def main():
     wizzard()
