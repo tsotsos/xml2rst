1c7e9b5956605fce3fbd6163ec7440879a33cba9
Georgios Tsotsos <tsotsos@gmail.com>
2019-08-08 21:41:07 +0300

    Template file for Sphinx conf.py.
    Adds a template file for Sphinx's conf.py. This template contains
    all settings from XML2RST that will allow to generate on "make"
    the doxygen xml output, will read the corresponding configuration file
    (VERSIONS). Also adds the project name, versions etc into conf.py that
    will allow to configure only VERSIONS file when the project is moving
    into new version.
    
    Signed-off-by: Georgios Tsotsos <tsotsos@gmail.com>

--- configs/conf.py
+++ configs/conf.py
@@ -1,5 +1,25 @@
+#START-XML2RST
+# Configuration file from XML2RST.
+#
+# This file is totaly compatible with conf.py of Sphinx. In case you change code
+# take care the imports we already import os, subprossess here.
+
+
 import os
-from subprocess import call
+import subprocess
+
+def yes_or_no(question):
+    answer = input(question + "(y/n): ").lower().strip()
+    print("")
+    while not(answer == "y" or answer == "yes" or \
+    answer == "n" or answer == "no"):
+        print("Answer with a yes or no!")
+        answer = input(question + "(y/n):").lower().strip()
+        print("")
+    if answer[0] == "y":
+        return True
+    else:
+        return False
 
 def simple_conf_parse ( cf ) :
     results = dict()
@@ -17,55 +37,101 @@ def simple_conf_parse ( cf ) :
             line = f.readline()
     return results
 
+def find_file(filename):
+
+    folder = os.getcwd()
+
+    while folder != "/" and filename not in os.listdir(folder):
+        folder = os.path.abspath(folder + "/../")
+
+    filepath = os.path.join(folder,filename)
+    if os.path.isfile(filepath):
+        return filepath
+
+    return False
+
 def get_configs():
-    # check for XML2RST conf file first
-    xml2rst_file = "XML2RST"
-    # try to add one folder up
-    if not os.path.isfile(xml2rst_file):
-        xml2rst_file = "../"+ xml2rst_file
 
-    xml2rst = simple_conf_parse(xml2rst_file)
-    if not xml2rst:
-        return False
+    version_data = dict()
 
-    # check for versions file
-    version_file = xml2rst["VERSIONS"]
+    # first of all we need to identify VERSIONS file we trust only this to
+    # find paths. Intially they are relative but we turn them into absolute
+    # to ensure shpinx proper execution
 
-    # try to add one folder up
-    if not os.path.isfile(version_file):
-        version_file = "../"+ version_file
+    version_file = find_file("VERSIONS")
+    # rootpath is nessessary to create paths
+    rootpath = os.path.dirname(version_file)
+    # rootpath = os.path.relpath(rootpath,os.getcwd())
 
+    # find doxyfile
+    doxyfile_file = find_file("Doxyfile")
+    #doxyfile_file = os.path.relpath(doxyfile_file,os.getcwd())
+    # parse doxyfile for output folder
+    doxyfile_data = simple_conf_parse(doxyfile_file)
+    doxyfile_dir  = os.path.dirname(doxyfile_file)
+    if doxyfile_data["OUTPUT_DIRECTORY"] == ".":
+        doxyfile_out_dir = os.path.abspath(doxyfile_dir)
+    else:
+        doxyfile_out_dir = os.path.relpath(
+                                doxyfile_data["OUTPUT_DIRECTORY"],
+                                doxyfile_dir)
+        doxyfile_out_dir = os.path.abspath(doxyfile_out_dir)
+
+    doxyfile_xml_dir = doxyfile_out_dir+"/"+doxyfile_data["XML_OUTPUT"]
+    # parse version_file values to dict()
     version_data = simple_conf_parse(version_file)
     if not version_data:
         return False
-
-    result = { **xml2rst,**version_data }
-    if not result:
+    if version_data is None:
         return False
-    if result is None:
-        return False
-    return result
 
-configs = get_configs()
-if not configs:
-    print ("Error! Please check your configuration files XML2RST in doc path and VERSIONS in root path")
+    # check for source path
+    srcpath = rootpath+"/"+version_data["SRC"]
+    srcpath = os.path.abspath(srcpath)
+    if os.path.isdir(srcpath):
+        version_data.update({"SRC":srcpath})
+    else:
+        return False
+
+    # check for docs path
+    docspath = rootpath+"/"+version_data["DOCS"]
+    docspath = os.path.abspath(docspath)
+    if os.path.isdir(srcpath):
+        version_data.update({"DOCS":docspath})
+    else:
+        return False
+
+    version_data.update({"DOXYFILE_XML":doxyfile_xml_dir})
+    version_data.update({"ROOT":rootpath})
+    version_data.update({"DOXYFILE":doxyfile_file})
+    version_data.update({"READTHEDOCS":True})
+    return version_data
+
+config = get_configs()
+if not config:
+    print ("Error! Please check your VERSIONS file in root path")
     exit()
-
-# doxyfile is always at the same path with XML2RST conf so:
-doxyfile = configs["XML2RST"].replace("XML2RST","Doxyfile")
-pritn(doxyfile)
-
-# doxygen generation
-# call(['doxygen', 'Doxyfile'])
+# Ask to run doxygen XML generation
+if yes_or_no("Do you want to run doxygen XML generation?"):
+    subprocess.call(["doxygen",config["DOXYFILE"]]
+                    ,cwd=config["DOCS"]
+                    ,stdout=subprocess.DEVNULL)
+else:
+    #if not os.path.isdir()
+    print("ok we will continue...")
 
 # generate sources
 
-breathe_projects = { "myproject" : "xml/" }
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if on_rtd:
-  html_theme = 'default'
-else:
+#END-XML2RST
+
+#START-BREATH
+# Breath settings for Sphinx
+
+extensions += ['breathe']
+breathe_projects = {config["PROJECT_NAME"]:config["DOXYFILE_XML"]}
+if config["READTHEDOCS"]:
   import sphinx_rtd_theme
   html_theme = "sphinx_rtd_theme"
   html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-breathe_default_project = "myproject"
+breathe_default_project = config["PROJECT_NAME"]
+#END-BREATH
